182eb5d5f7085b956d7e0c12529e85295255eff6
zura <71590934+avalianiz@users.noreply.github.com>
2026-05-19 01:42:49 +0400

    feat: added solutions for homework #2. (#1)

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // print full name
+        Console.WriteLine("Zurab Avaliani");
+        
+        // make console color blue
+        Console.ForegroundColor = ConsoleColor.Blue;
+        
+        // get input from user and return it
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}!");
     }
 }
