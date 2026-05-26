ce0e8efba8c9f3ffdb8d3421fcf1109c01aeef0e
avalianiz <avalianizurab2006@gmail.com>
2026-05-26 20:06:50 +0400

    feat: add task 1 implementation

---
+++ homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DivideByFive();
+    }
+
+    
+    #region divideby5
+    static void DivideByFive()
+    {
+        Console.WriteLine("Input a number: ");
+        if (int.TryParse(Console.ReadLine(), out int input))
+        {
+            Console.WriteLine(input % 5 == 0 ? "yes" : "no");
+        }
+        else
+        {
+            Console.WriteLine("please enter a valid number!");
+        }
+    }
+    #endregion
+    
+}
