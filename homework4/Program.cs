ce86bac99130ba9c23699e1eb8c700e4ba3e0d46
avalianiz <avalianizurab2006@gmail.com>
2026-06-01 18:21:09 +0400

    feat: add task 2

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Task1();
+        //Task1();
+        Task2();
     }
 
 
@@ -56,6 +57,87 @@ class Program
         for (int i = 0; i < oddCount; i++)
         {
             Console.Write(odds[i] + " ");
+        }
+    }
+
+
+    static void Task2()
+    {
+        var contacts = new Dictionary<string, string>();
+
+        while (true)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("1. add a contact");
+            Console.WriteLine("2. delete a contact");
+            Console.WriteLine("3. update a contact");
+            Console.WriteLine("4. view contacts");
+            Console.WriteLine("5. exit");
+            
+            Console.WriteLine("choose an option: ");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("enter name: ");
+                    var name = Console.ReadLine();
+                    
+                    Console.WriteLine("enter number: ");
+                    var phone = Console.ReadLine();
+                    
+                    contacts[name] = phone;
+                    Console.WriteLine("contact added");
+                    break;
+
+                case "2":
+                    Console.Write("enter name to delete: ");
+                    name = Console.ReadLine();
+
+                    if (contacts.Remove(name))
+                        Console.WriteLine("contact deleted");
+                    else
+                        Console.WriteLine("contact not found");
+
+                    break;
+
+                case "3":
+                    Console.Write("enter name to update: ");
+                    name = Console.ReadLine();
+
+                    if (contacts.ContainsKey(name))
+                    {
+                        Console.Write("enter new phone number: ");
+                        phone = Console.ReadLine();
+
+                        contacts[name] = phone;
+                        Console.WriteLine("updated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("contact not found");
+                    }
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("\ncontacts:");
+
+                    foreach (var contact in contacts)
+                    {
+                        Console.WriteLine($"{contact.Key}: {contact.Value}");
+                    }
+
+                    break;
+
+                case "5":
+                    return;
+
+                default:
+                    Console.WriteLine("invalid option");
+                    break;
+            }
+            
         }
     }
 }
