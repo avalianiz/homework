ad57e2049c22034fbdce1811e70fc7e9553a298c
avalianiz <avalianizurab2006@gmail.com>
2026-06-18 16:03:43 +0400

    add: task1

---
+++ homework9/LastLineReader.cs
@@ -0,0 +1,32 @@
+﻿namespace homework9;
+
+public class LastLineReader
+{
+    private static string filePath = 
+        Path.Combine(@"D:\RiderProjects\homework\homework9", "input.txt");
+    
+    internal static void GetUserInput()
+    {
+        Console.WriteLine("enter the amount of lines: ");
+        if (!int.TryParse(Console.ReadLine(), out var count))
+        {
+            Console.WriteLine("invalid input");
+            return;
+        }
+
+        string[] lines = new string[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            lines[i] = Console.ReadLine();
+        }
+        
+        File.WriteAllLines(filePath, lines);
+    }
+
+    internal static void ReadLastLine()
+    {
+        string[] lines = File.ReadAllLines(filePath);
+        Console.WriteLine($"Last line: {lines[^1]}");
+    }
+}
---
+++ homework9/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace homework9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task1();
+    }
+
+
+    private static void Task1()
+    {
+        LastLineReader.GetUserInput();
+        LastLineReader.ReadLastLine();
+    }
+}
