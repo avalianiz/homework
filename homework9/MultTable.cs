9c0c8468d1dd31068dab38e0994beea0fdf6d582
avalianiz <avalianizurab2006@gmail.com>
2026-06-18 16:45:12 +0400

    add: task 2

---
+++ homework9/MultTable.cs
@@ -0,0 +1,25 @@
+﻿namespace homework9;
+
+public class MultTable
+{
+    private static string filePath = 
+        Path.Combine(@"D:\RiderProjects\homework\homework9", "table.txt");
+    
+    
+    internal static void PrintTable()
+    {
+        Console.WriteLine("enter N: ");
+        if (!int.TryParse(Console.ReadLine(), out var n))
+        {
+            Console.WriteLine("invalid input");
+            return;
+        }
+
+        using StreamWriter streamWriter = new StreamWriter(filePath);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            streamWriter.WriteLine($"{n} * {i} = {n * i}");
+        }
+    }
+}
--- homework9/Program.cs
+++ homework9/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Task1();
+        //Task1();
+        MultTable.PrintTable();
     }
 
 
