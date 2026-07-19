ba03d5f6b63474616fd244697548d042769dce1d
avalianiz <avalianizurab2006@gmail.com>
2026-07-19 15:34:41 +0400

    feat(models): add person and address models

---
+++ homework14/Models/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace homework14.Models;
+
+public class Address
+{
+    public string Country { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string HomeNumber { get; set; } = string.Empty;
+}
---
+++ homework14/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace homework14.Models;
+
+public class Person
+{
+    public DateTime CreateDate { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string JobPosition { get; set; } = string.Empty;
+    public double Salary  { get; set; }
+    public double? WorkExperience { get; set; }
+    public Address? PersonAddress { get; set; }
+}
