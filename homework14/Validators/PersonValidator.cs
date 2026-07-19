4aea8d14c52ca7a9d87b8bdc436b814c97144b5c
avalianiz <avalianizurab2006@gmail.com>
2026-07-19 19:02:13 +0400

    feat(validator): add person validator implemented createdate restrictions only

---
+++ homework14/Validators/PersonValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using homework14.Models;
+
+namespace homework14.Validators;
+
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(person => person.CreateDate)
+            .NotEmpty()
+            .WithMessage("Create date is required.")
+            .LessThanOrEqualTo(DateTime.Today)
+            .WithMessage("Create date can not be in the future.");
+
+    }
+}
