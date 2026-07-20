beaf8d70c451cd6b5a3477ae82e0c9616b2c66d5
avalianiz <avalianizurab2006@gmail.com>
2026-07-20 16:19:41 +0400

    feat(services): add service for working with json itself and interface

---
+++ homework14/Services/IPersonFileService.cs
@@ -0,0 +1,11 @@
+﻿using homework14.Models;
+
+namespace homework14.Services;
+
+public interface IPersonFileService
+{
+    Task<List<Person>> GetAllAsync();
+    Task<List<Person>> AddAsync(Person person);
+    Task<List<Person>> DeleteAsync(int id);
+    Task<List<Person>> ReplaceAsync(int id, Person person);
+}
